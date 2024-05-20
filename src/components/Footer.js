15167c59131709ad4035328081459ffb9d92d7b0
anayag33 <anayagoradia2012@gmail.com>
2024-05-20 15:08:54 -0700

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,16 +4,26 @@ import TwitterIcon from '@mui/icons-material/Twitter'
 import FacebookIcon from '@mui/icons-material/Facebook'
 import LinkedInIcon from '@mui/icons-material/LinkedIn'
 import '../styles/Footer.css'
+import { Link } from "react-router-dom";
+
 
 
 function Footer() {
   return (
     <div className='footer'>
       <div className='socialMedia'>
-        <InstagramIcon />
-        <TwitterIcon />
-        <FacebookIcon />
-        <LinkedInIcon />
+        <Link to='https://www.instagram.com/'>
+          <InstagramIcon />
+        </Link>
+        <Link to='https://twitter.com/?lang=en'>
+          <TwitterIcon />
+        </Link>
+        <Link to='https://www.facebook.com/'>
+          <FacebookIcon />
+        </Link>
+        <Link to='https://www.linkedin.com/feed/'>
+          <LinkedInIcon />
+        </Link>
         <p>&copy; 2024 anayasarcade.com</p>
       </div>
     </div>
